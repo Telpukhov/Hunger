e80ee6a0776361c57163e32a127fa88a41f58202
Telpukhov <nofrost@inbox.ru>
2017-10-07 01:52:48 +0400

    slowly moving

--- hunger.py
+++ hunger.py
@@ -16,22 +16,26 @@ class game(object):
         self.__open__ = 1
         self.__started__ = 0
         self.__players__ = {}
-        self.__player_num__ = 0
         self.__host__ = 0
 
-    def add_player(self, adr, name):
-        self.__players__[self.__player_num__] = {
-                                        'adr': adr,
-                                        'name': name,
-                                      }
-        self.__player_num__ += 1
+    def add_player(self, adr, name, chat_id, message_id):
+        self.__players__[adr] = (name, chat_id, message_id)
+
+    def say2all(self, text, keyboard):
+        for player in self.__players__:
+            name = self.__players__[player][0]
+            chat_id = self.__players__[player][1]
+            message_id = self.__players__[player][2]
+            bot.editMessageText((chat_id, message_id), text,
+                                reply_markup=keyboard)
+
 
 # =====================================================
 #                 ОБРАБОТЧИК СООБЩЕНИЙ
 # =====================================================
 
 def msg_handler(adr, msg, name, chat_id, message_id):
-    if adr not in playing_people:
+    if adr not in in_lobby:
         bot.sendMessage(adr, 'Добро пожаловать в ППЦ!!!', reply_markup=welcome_keybord())
 
 
@@ -42,7 +46,7 @@ def keyboard_handler(query_id, adr, name, msg, chat_id, message_id):
     if msg == 'enter':
         #bot.deleteMessage((chat_id, message_id))
         #bot.sendMessage(adr, 'Добро пожаловать в ППЦ!!!', reply_markup=first_menu())
-        playing_people[adr] = (None, chat_id, message_id)
+        in_lobby[adr] = (None, chat_id, message_id)
         bot.editMessageText((chat_id, message_id), 'Подключитесь к созданной игре, или создайте сами', reply_markup=first_menu())
 
     #_____________________________________
@@ -51,12 +55,16 @@ def keyboard_handler(query_id, adr, name, msg, chat_id, message_id):
         host = adr
         games[name] = game()
         games[name].__host__ = host
-        games[name].add_player(host, name)
-        playing_people[host] = (name, chat_id, message_id)
+        games[name].add_player(host, name, chat_id, message_id)
+        in_lobby[host] = (name, chat_id, message_id)
         hosts[host] = name
         #bot.deleteMessage((chat_id, message_id))
         #bot.sendMessage(adr, 'Поздравляю! Вы создали игру! Теперь вы можете проверить подключенных игроков и начать!', reply_markup=host_keyboard())
         bot.editMessageText((chat_id, message_id), 'Поздравляю! Вы создали игру! Теперь вы можете проверить подключенных игроков и начать!', reply_markup=host_keyboard())
+        for player in in_lobby:
+            if player not in hosts:
+                bot.editMessageText((in_lobby[player][1], in_lobby[player][2]), 'Подключитесь к созданной игре, или создайте сами',
+                                    reply_markup=first_menu())
 
     #_____________________________________
     #    ПРОВЕРКА ПОДКЛЮЧЕННЫХ ИГРОКОВ
@@ -65,7 +73,7 @@ def keyboard_handler(query_id, adr, name, msg, chat_id, message_id):
             g = hosts[adr]
             pl = ''
             for i in range(games[g].__player_num__):
-                pl += games[g].__players__[i]['name'] + ' '
+                pl += games[g].__players__[i]['name'] + '   '
             bot.answerCallbackQuery(query_id, pl, True)
 
     # _____________________________________
@@ -73,22 +81,41 @@ def keyboard_handler(query_id, adr, name, msg, chat_id, message_id):
     if msg == 'back2main_menu':
         if adr in hosts:
             hosts.pop(adr)
-            playing_people[adr] = (None, chat_id, message_id)
+            in_lobby[adr] = (None, chat_id, message_id)
             games.pop(name)
             #bot.deleteMessage((chat_id, message_id))
             #bot.sendMessage(adr, 'Добро пожаловать в ППЦ!!!', reply_markup=first_menu())
             bot.editMessageText((chat_id, message_id), 'Добро пожаловать в ППЦ!!!', reply_markup=first_menu())
+            for player in in_lobby:
+                if player not in hosts:
+                    if in_lobby[player][0] == name:
+                        in_lobby[player] = (None, in_lobby[player][1], in_lobby[player][2])
+                    bot.editMessageText((in_lobby[player][1], in_lobby[player][2]), 'Подключитесь к созданной игре, или создайте сами',
+                                        reply_markup=first_menu())
+
 
     # _____________________________________
     #      ПОДКЛЮЧЕНИЕ К ИГРЕ
     print(msg)
     if 'connect_' in msg:
         gm2connect = msg[8:]
-        playing_people[adr] = (gm2connect,chat_id, message_id)
-        games[gm2connect].add_player(adr, name)
+        in_lobby[adr] = (gm2connect,chat_id, message_id)
+        games[gm2connect].add_player(adr, name, chat_id, message_id)
         #bot.deleteMessage((chat_id, message_id))
         #bot.sendMessage(adr, 'Вы присоединились к игре ' + gm2connect, reply_markup=first_menu())
         bot.editMessageText((chat_id, message_id), 'Вы присоединились к игре ' + gm2connect, reply_markup=first_menu())
+
+
+    # _____________________________________
+    #              СТАРТ ИГРЫ
+    if msg == 'start_game':
+        for player in in_lobby:
+            if in_lobby[player][0] == name:
+                in_game[player] = in_lobby[player]
+        for player in games[name].__players__:
+            in_lobby.pop(player)
+        games[name].__open__ = 0
+        games[name].say2all('РАБОТАЕТ', welcome_keybord())
 
 
 # =====================================================
@@ -106,7 +133,7 @@ def first_menu():
 def host_keyboard():
     buttons = list()
     buttons.append([InlineKeyboardButton(text='ИГРОКИ В ЛОББИ', callback_data='check_players')])
-    buttons.append([InlineKeyboardButton(text='СТАРТ', callback_data='ignore')])
+    buttons.append([InlineKeyboardButton(text='СТАРТ', callback_data='start_game')])
     buttons.append([InlineKeyboardButton(text='ОБРАТНО', callback_data='back2main_menu')])
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
@@ -123,9 +150,11 @@ def welcome_keybord():
 
 global games
 global hosts
-global playing_people
+global in_lobby
+global in_game
 games = {}
-playing_people = {}
+in_lobby = {}
+in_game = {}
 hosts = {}
 games['Hello'] = game()
 games['Slavik'] = game()
@@ -148,7 +177,7 @@ while 1:
     pkgs = bot.getUpdates(offset = offset)
     if pkgs != []: # сообщение/я пришло... обрабатываем
         for pkg in pkgs:    # обрабатываем последовательно
-            pprint(pkg)
+            #pprint(pkg)
             if 'message' in pkg: # пришло текстовое сообщение
                 adr = pkg['message']['from']['id']
                 msg = pkg['message']['text']
@@ -166,16 +195,19 @@ while 1:
                 msg = pkg['callback_query']['data']
                 keyboard_handler(query_id, adr, name, msg, chat_id, message_id)
         offset = int(pkgs[-1].get('update_id')) + 1
+    print('========')
     print('games')
     pprint(games)
-    print('playing_people')
-    pprint(playing_people)
+    print('in_lobby')
+    pprint(in_lobby)
+    print('in_game')
+    pprint(in_game)
     print('hosts')
     pprint(hosts)
     #print(k)
     #if k % 13 == 0:
-    #    for add in playing_people:
-    #        chat_id = playing_people[add][1]
-    #        message_id = playing_people[add][2]
+    #    for add in in_lobby:
+    #        chat_id = in_lobby[add][1]
+    #        message_id = in_lobby[add][2]
     #        bot.deleteMessage((chat_id, message_id))
     time.sleep(1)
