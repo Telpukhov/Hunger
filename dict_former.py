004c525773bd3d1774b7d00c532aae87c5272124
plaksivayatryapka <boiler00@mail.ru>
2017-11-03 01:07:55 +0300

    Update dict_former.py

--- dict_former.py
+++ dict_former.py
@@ -2,19 +2,17 @@ import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
 
-
 def transpose(word):
-    if word[0] == 'п' or word[0] == 'П':
+    if word[0].lower() == 'п':
         if 'NOUN' in morph.parse(word)[0].tag:
             new_word = morph.parse(word)[0].normal_form
             print(new_word)
-
-    if word[0] == 'п' or word[0] == 'П':
-        if 'ADJF' in morph.parse(word)[0].tag:
+        
+        elif 'ADJF' in morph.parse(word)[0].tag:
             new_word = morph.parse(word)[0].inflect({'plur', 'gent'}).word
             print(new_word)
 
-    if word[0] == 'ц' or word[0] == 'Ц':
+    if word[0].lower() == 'ц':
         if 'NOUN' in morph.parse(word)[0].tag:
             new_word = morph.parse(word)[0].inflect({'plur', 'gent'}).word
             print(new_word)
